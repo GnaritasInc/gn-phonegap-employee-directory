57a1d311524421b4b8173d7eb1893b0c9a99d6db
David Santucci <dsantucci@gnaritas.com>
2014-08-25 16:29:47 -0400

    Added native alert.

--- js/main.js
+++ js/main.js
@@ -13,8 +13,19 @@ var app = {
         });
     },
 
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
+
     initialize: function() {
-        this.store = new LocalStorageStore();
+        var self = this;
+        this.store = new LocalStorageStore(function () {
+        	self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
