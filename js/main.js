487078a17ffa9e85ee6533906e02715839f4e1e7
David Santucci <dsantucci@gnaritas.com>
2014-08-25 17:23:10 -0400

    Added HomeView class.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 var app = {
 
-    findByName: function() {
-	    var self = this;
-	    this.store.findByName($('.search-key').val(), function(employees) {
-		$('.employee-list').html(self.employeeLiTpl(employees));
-	    });
-    },
 
 	showAlert: function (message, title) {
 		if (navigator.notification) {
@@ -15,22 +9,12 @@ var app = {
 		}
 	},
 
-	renderHomeView: function() {
-	    $('body').html(this.homeTpl());
-	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-
-    initialize: function() {
-        var self = this;
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-	this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-        
-        this.store = new LocalStorageStore(function () {
-        	self.renderHomeView();
-        });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
-
+	initialize: function() {
+	    var self = this;
+	    this.store = new LocalStorageStore(function() {
+		$('body').html(new HomeView(self.store).render().el);
+	    });
+	}
 };
 
 app.initialize();
