fd16774fc13842f36ff74555741b8150e6030dc5
David Santucci <dsantucci@gnaritas.com>
2014-08-25 16:52:57 -0400

    Added template for home view.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+	    var self = this;
+	    this.store.findByName($('.search-key').val(), function(employees) {
+		$('.employee-list').html(self.employeeLiTpl(employees));
+	    });
     },
 
 	showAlert: function (message, title) {
@@ -21,10 +15,18 @@ var app = {
 		}
 	},
 
+	renderHomeView: function() {
+	    $('body').html(this.homeTpl());
+	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
+
     initialize: function() {
         var self = this;
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+	this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        
         this.store = new LocalStorageStore(function () {
-        	self.showAlert('Store Initialized', 'Info');
+        	self.renderHomeView();
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
