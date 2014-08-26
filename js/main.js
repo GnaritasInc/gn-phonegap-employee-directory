d73805f343afb195fd13b8b5acb9e2310aeee61d
David Santucci <dsantucci@gnaritas.com>
2014-08-26 10:22:51 -0400

    Added touch styles.

--- js/main.js
+++ js/main.js
@@ -14,6 +14,30 @@ var app = {
 	    this.store = new LocalStorageStore(function() {
 		$('body').html(new HomeView(self.store).render().el);
 	    });
+	    
+	    this.registerEvents();
+	},
+	
+	registerEvents: function() {
+	    var self = this;
+	    // Check of browser supports touch events...
+	    if (document.documentElement.hasOwnProperty('ontouchstart')) {
+		// ... if yes: register touch event listener to change the "selected" state of the item
+		$('body').on('touchstart', 'a', function(event) {
+		    $(event.target).addClass('tappable-active');
+		});
+		$('body').on('touchend', 'a', function(event) {
+		    $(event.target).removeClass('tappable-active');
+		});
+	    } else {
+		// ... if not: register mouse events instead
+		$('body').on('mousedown', 'a', function(event) {
+		    $(event.target).addClass('tappable-active');
+		});
+		$('body').on('mouseup', 'a', function(event) {
+		    $(event.target).removeClass('tappable-active');
+		});
+	    }
 	}
 };
 
