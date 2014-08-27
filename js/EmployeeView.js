22ca9f5ac60b09af9a2f90113191bffd3930fe2c
David Santucci <dsantucci@gnaritas.com>
2014-08-27 11:17:40 -0400

    Added contact support.

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -3,6 +3,7 @@ var EmployeeView = function(employee) {
 	this.initialize = function() {
 		this.el = $('<div/>');
 		this.el.on('click', '.add-location-btn', this.addLocation);
+		this.el.on('click', '.add-contact-btn', this.addToContacts);
 	};
 	
 	this.render = function() {
@@ -21,6 +22,23 @@ var EmployeeView = function(employee) {
 		    // alert('Error getting location');
 		    app.showAlert('Error getting location', 'Error');
 		});
+	    return false;
+	};
+
+	this.addToContacts = function(event) {
+	    event.preventDefault();
+	    console.log('addToContacts');
+	    if (!navigator.contacts) {
+		app.showAlert("Contacts API not supported", "Error");
+		return;
+	    }
+	    var contact = navigator.contacts.create();
+	    contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+	    var phoneNumbers = [];
+	    phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+	    phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
+	    contact.phoneNumbers = phoneNumbers;
+	    contact.save();
 	    return false;
 	};
 
